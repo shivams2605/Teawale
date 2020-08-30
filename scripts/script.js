aea5a786f1ec0a44c537e51d842136174b0ed121
EM\SSachdeva <Shivam.Sachdeva@EllieMae.com>
2020-08-30 21:15:49 +0530

    Teawale

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+var header = '<div class="pull-left logo-container"><h1 class="logo-text">Teawale<span>.</span><span>.</span><span>.</span></h1></div><div class="pull-right header-cta-container"><a class="header-cta" href="index.html">Home</a><a class="header-cta" href="about_us.html">About us</a><a class="header-cta" href="policies.html">Policies</a><a class="header-cta" href="contact_us.html">Contact us</a></div>';
+var footer = '<span>&copy; 2020 Teawale. All rights reserved</span>'
+
+
+
+document.getElementById("header").innerHTML=header;
+document.getElementById("footer").innerHTML=footer;
